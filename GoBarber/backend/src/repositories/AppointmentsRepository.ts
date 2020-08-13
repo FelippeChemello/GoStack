a30f7955ea0d9e700abbcbc087dff577c8195a5c
Felippe Chemello <felippe.chemello@kinghost.com.br>
2020-08-12 21:34:13 -0300

    Aula 06 - GoBarber backend - Iniciando Repositorio (Manipulaçao de Dados) e Desacoplando codigo da Rota

---
+++ GoBarber/backend/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,32 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+    private appointments: Appointment[];
+
+    constructor() {
+        this.appointments = [];
+    }
+
+    public create(provider: string, date: Date): Appointment {
+        const appointment = new Appointment(provider, date);
+
+        this.appointments.push(appointment);
+
+        return appointment;
+    }
+
+    public findByDate(date: Date): Appointment | null {
+        const findAppointment = this.appointments.find(appointment =>
+            isEqual(date, appointment.date),
+        );
+
+        return findAppointment || null;
+    }
+}
+
+export default AppointmentsRepository;
+
+// Repositório serve como um caminho entre a aplicação e a persistencia
+// Nele serão criados os métodos para criar, deletar, buscar etc. os dados
+// DETENTOR DAS OPERAÇÕES SOBRE OS DADOS DA APLICAÇÃO
