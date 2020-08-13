6856778cec981295bf4ae76da09bac786ccce8bc
Felippe Chemello <felippe.chemello@kinghost.com.br>
2020-08-12 21:39:10 -0300

    Aula 07 - GoBarber backend - Listagem de todos os dados salvos

--- GoBarber/backend/src/repositories/AppointmentsRepository.ts
+++ GoBarber/backend/src/repositories/AppointmentsRepository.ts
@@ -8,6 +8,10 @@ class AppointmentsRepository {
         this.appointments = [];
     }
 
+    public all(): Appointment[] {
+        return this.appointments;
+    }
+
     public create(provider: string, date: Date): Appointment {
         const appointment = new Appointment(provider, date);
 
--- GoBarber/backend/src/routes/appointments.routes.ts
+++ GoBarber/backend/src/routes/appointments.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, response } from 'express';
 import { startOfHour, parseISO } from 'date-fns';
 // parseISO converte String para Objeto JS do tipo Date
 // startOfHour define a Hora do Objeto Date como inicio (minuto 0, segundo 0 etc.)
@@ -7,6 +7,12 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+    const appointments = appointmentsRepository.all();
+
+    return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
     // provider = Nome do prestador de serviços
