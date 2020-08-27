fc57c76651aa7e9ac165f2c0c2c798e2f5250f9f
Felippe Chemello <felippe.chemello@kinghost.com.br>
2020-08-27 17:07:35 -0300

    edit build command and add create database

--- GoBarber/backend/src/database/migrations/1597968397301-CreateAppointments.ts
+++ GoBarber/backend/src/database/migrations/1597968397301-CreateAppointments.ts
@@ -3,6 +3,8 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 export default class CreateAppointments1597968397301
     implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createDatabase('gostack_gobarber');
+
         await queryRunner.createTable(
             new Table({
                 name: 'appointments',
@@ -30,5 +32,7 @@ export default class CreateAppointments1597968397301
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.dropTable('appointments');
+
+        await queryRunner.dropDatabase('gostack_gobarber')
     }
 }
