e3e0e0c529099d8a928e30de0d95882ee347b873
Felippe <felippe.chemello@kinghost.com.br>
2020-10-04 17:27:23 -0300

    Aula 18 - backend|database - Servindo arquivos estaticos (avatar) e realizando o update do avatar

---
+++ GoBarber/backend/src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import fs from 'fs';
+import { getRepository } from 'typeorm'
+
+import uploadCOnfig from '../config/upload'
+import User from '../models/User'
+
+interface Request {
+    user_id: string,
+    avatarFileName: string
+}
+
+class UpdateUserAvatarService {
+    public async execute({ user_id, avatarFileName }: Request): Promise<User> {
+        const userRepository = getRepository(User)
+
+        const user = await userRepository.findOne(user_id)
+
+        if (!user) {
+            throw new Error('Only authenticated users can change avatar')
+        }
+
+        if (user.avatar) {
+            const userAvatarFilePath = path.join(uploadCOnfig.directory, user.avatar)
+
+            const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath)
+
+            if (userAvatarFileExists) {
+                await fs.promises.unlink(userAvatarFilePath)
+            }
+        }
+
+        user.avatar = avatarFileName;
+
+        await userRepository.save(user)
+
+        return user
+    }
+}
+
+export default UpdateUserAvatarService;
