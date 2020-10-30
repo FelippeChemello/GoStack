4d9ef987c9364803d359b7765e8148621f5dd796
Felippe <felippe.chemello@kinghost.com.br>
2020-10-29 21:01:23 -0300

    Aula 01 - Fundamentos ReactJS - Rotas

---
+++ GithubExplorer/src/pages/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dashboard: React.FunctionComponent = () => {
+    return <h1> Dashboard </h1>;
+};
+
+export default Dashboard;
---
+++ GithubExplorer/src/pages/Repository/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// FunctionComponent pode ser escrito como FC
+const Repository: React.FunctionComponent = () => {
+    return <h1> Repository </h1>;
+};
+
+export default Repository;
