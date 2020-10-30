91856943656dfb5ba521f09a4d4cb5d78cfb507d
Felippe <felippe.chemello@kinghost.com.br>
2020-10-29 21:52:07 -0300

    Aula 03 - Fundamentos ReactJS - Estilizando o dashboard

--- GithubExplorer/src/pages/Dashboard/index.tsx
+++ GithubExplorer/src/pages/Dashboard/index.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FunctionComponent = () => {
-    return <Title> Explore repositórios no Github </Title>;
+    return (
+        <>
+            <img src={logoImg} alt="Github explorer" />
+            <Title> Explore repositórios no Github </Title>
+
+            <Form action="">
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit"> Pesquisar </button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img src="https://picsum.photos/200" alt="Profile" />
+                    <div>
+                        <strong>Nome</strong>
+                        <p>Descrição</p>
+                    </div>
+
+                    <FiChevronRight size={20} color="#cbcd6" />
+                </a>
+
+                <a href="teste">
+                    <img src="https://picsum.photos/200" alt="Profile" />
+                    <div>
+                        <strong>Nome</strong>
+                        <p>Descrição</p>
+                    </div>
+
+                    <FiChevronRight size={20} color="#cbcd6" />
+                </a>
+
+                <a href="teste">
+                    <img src="https://picsum.photos/200" alt="Profile" />
+                    <div>
+                        <strong>Nome</strong>
+                        <p>Descrição</p>
+                    </div>
+
+                    <FiChevronRight size={20} color="#cbcd6" />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
