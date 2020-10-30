a69159e8c680db967fda68fda3d0281f0308aa6c
Felippe <felippe.chemello@kinghost.com.br>
2020-10-30 15:33:07 -0300

    Aula 08 - Fundamentos ReactJS - Estilo da Pagina de Detalhes do Repositorio

--- GithubExplorer/src/pages/Repository/index.tsx
+++ GithubExplorer/src/pages/Repository/index.tsx
@@ -1,8 +1,60 @@
 import React from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
-// FunctionComponent pode ser escrito como FC
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfos, Issues } from './styles';
+
 const Repository: React.FunctionComponent = () => {
-    return <h1> Repository </h1>;
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={20} />
+                    Voltar
+                </Link>
+            </Header>
+            <RepositoryInfos>
+                <header>
+                    <img src="https://picsum.photos/200" alt="" />
+                    <div>
+                        <strong>FelippeChemello/FelippeChemello</strong>
+                        <p>Descrição do Repo</p>
+                    </div>
+                </header>
+
+                <ul>
+                    <li>
+                        <strong> 1808 </strong>
+                        <p> Stars </p>
+                    </li>
+
+                    <li>
+                        <strong> 48 </strong>
+                        <p> Forks </p>
+                    </li>
+
+                    <li>
+                        <strong> 67 </strong>
+                        <p> Issues abertas </p>
+                    </li>
+                </ul>
+            </RepositoryInfos>
+
+            <Issues>
+                <Link to="github">
+                    <div>
+                        <strong>Nome da Issue</strong>
+                        <p> Autor da Issue </p>
+                    </div>
+
+                    <FiChevronRight size={32} />
+                </Link>
+            </Issues>
+        </>
+    );
 };
 
 export default Repository;
---
+++ GithubExplorer/src/pages/Repository/styles.ts
@@ -0,0 +1,124 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    a {
+        font-size: 16px;
+        line-height: 19px;
+        color: #a8a8b3;
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+
+        &:hover {
+            color: ${shade(0.5, '#a8a8b3')};
+        }
+    }
+`;
+
+export const RepositoryInfos = styled.div`
+    margin-top: 36px;
+
+    header {
+        margin-top: 70px;
+        display: flex;
+        align-items: center;
+
+        img {
+            border-radius: 50%;
+            height: 120px;
+            width: 120px;
+        }
+
+        div {
+            margin: 0 32px;
+
+            strong {
+                font-weight: bold;
+                font-size: 36px;
+                line-height: 42px;
+                color: #3d3d4d;
+            }
+
+            p {
+                margin-top: 12px;
+                font-size: 20px;
+                line-height: 23px;
+                color: #737380;
+            }
+        }
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+        margin-top: 40px;
+
+        li {
+            & + li {
+                margin-left: 80px;
+            }
+
+            strong {
+                font-weight: bold;
+                font-size: 36px;
+                line-height: 42px;
+                color: #3d3d4d;
+            }
+
+            p {
+                font-size: 20px;
+                line-height: 23px;
+                color: #6c6c80;
+                margin-top: 4px;
+            }
+        }
+    }
+`;
+
+export const Issues = styled.div`
+    margin-top: 70px;
+
+    a {
+        min-height: 112px;
+        width: 100%;
+        background: #fff;
+        border-radius: 5px;
+        padding: 28px;
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        transition: transform 0.2s;
+
+        &:hover {
+            transform: translateX(10px);
+        }
+
+        & + a {
+            margin-top: 16px;
+        }
+
+        strong {
+            font-weight: bold;
+            font-size: 24px;
+            line-height: 28px;
+            color: #3d3d4d;
+        }
+
+        p {
+            margin-top: 8px;
+            font-size: 18px;
+            line-height: 21px;
+            color: #a8a8b3;
+        }
+
+        svg {
+            margin-left: auto;
+            color: #c9c9d4;
+        }
+    }
+`;
