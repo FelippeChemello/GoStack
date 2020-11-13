43d3f26af5e2ddbe72fc5b32ae9765ddfe1a9073
Felippe Chemello <felippechemello@gmail.com>
2020-11-12 21:03:15 -0300

    Aula 07 - Frontend GoBarber - Utilizando useRef e useCallback do React

--- GoBarber/web/src/components/Input/index.tsx
+++ GoBarber/web/src/components/Input/index.tsx
@@ -1,4 +1,10 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import React, {
+    InputHTMLAttributes,
+    useEffect,
+    useRef,
+    useState,
+    useCallback,
+} from 'react';
 import { IconBaseProps } from 'react-icons/lib';
 import { useField } from '@unform/core';
 
@@ -13,7 +19,10 @@ const Input: React.FC<InputProps> = ({ icon: Icon, ...attributes }) => {
     const { fieldName, defaultValue, error, registerField } = useField(
         attributes.name,
     );
-    const inputRef = useRef(null);
+    // inputRef atribui uma referencia a um elemento. O elemento fica salvo em inputRef.current
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [isFocused, setIsFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
 
     useEffect(() => {
         registerField({
@@ -23,10 +32,28 @@ const Input: React.FC<InputProps> = ({ icon: Icon, ...attributes }) => {
         });
     }, [fieldName, registerField]);
 
+    // useCallback cria a função e salva na memória, dessa forma, quando recarregar o componente não irá recriar a função,
+    // no segundo parametro funciona como useEffect (Recria apenas quando aquelas variaveis forem modificadas)
+    const handleInputBlur = useCallback(() => {
+        setIsFocused(false);
+
+        setIsFilled(Boolean(inputRef.current?.value));
+    }, []);
+
+    const handleInputFocus = useCallback(() => {
+        setIsFocused(true);
+    }, []);
+
     return (
-        <Container>
+        <Container isFocused={isFocused} isFilled={isFilled}>
             {Icon && <Icon size={20} />}
-            <input defaultValue={defaultValue} ref={inputRef} {...attributes} />
+            <input
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                defaultValue={defaultValue}
+                ref={inputRef}
+                {...attributes}
+            />
         </Container>
     );
 };
