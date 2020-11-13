9106625d203e1574bc5e8d4f93937e0dff85612f
Felippe Chemello <felippechemello@gmail.com>
2020-11-12 21:12:14 -0300

    Aula 08 - Frontend GoBarber - Validaçao de formularios com Yup

--- GoBarber/web/src/pages/SignUp/index.tsx
+++ GoBarber/web/src/pages/SignUp/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Form } from '@unform/web';
-
+import * as Yup from 'yup';
 import { FiArrowLeft, FiLock, FiLogIn, FiMail, FiUser } from 'react-icons/fi';
+
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './style';
 
@@ -9,9 +10,23 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-    function handleSubmit(data: any): void {
-        console.log(data);
-    }
+    const handleSubmit = useCallback(async (data: any) => {
+        try {
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatório'),
+                email: Yup.string()
+                    .required('E-mail obrigatório')
+                    .email('Digite um e-mail válido'),
+                password: Yup.string().min(6, 'Minimo de 6 caracteres'),
+            });
+
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    }, []);
 
     return (
         <Container>
