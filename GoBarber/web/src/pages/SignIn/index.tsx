6adc33a3f33e98625cf68e6643dc030313d5155e
Felippe Chemello <felippechemello@gmail.com>
2020-11-12 21:57:18 -0300

    Aula 11 - Frontend GoBarber - Inserindo validaçoes, estilizaçoes e tooltip na pagina de login

--- GoBarber/web/src/pages/SignIn/index.tsx
+++ GoBarber/web/src/pages/SignIn/index.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
-
+import React, { useCallback, useRef } from 'react';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 import { FiLock, FiLogIn, FiMail } from 'react-icons/fi';
+
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './style';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 const SignIn: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
+
+    const handleSubmit = useCallback(async (data: any) => {
+        formRef.current?.setErrors({});
+
+        try {
+            const schema = Yup.object().shape({
+                email: Yup.string()
+                    .required('E-mail obrigatório')
+                    .email('Digite um e-mail válido'),
+                password: Yup.string().required('Senha obrigatória'),
+            });
+
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+        } catch (err) {
+            console.error(err);
+
+            const errors = getValidationErrors(err);
+
+            formRef.current?.setErrors(errors);
+        }
+    }, []);
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="GoBarber" />
 
-                <form>
+                <Form ref={formRef} onSubmit={handleSubmit}>
                     <h1>Faça seu login</h1>
 
                     <Input placeholder="E-mail" name="email" icon={FiMail} />
@@ -28,7 +57,7 @@ const SignIn: React.FC = () => {
                     <Button type="submit"> Entrar </Button>
 
                     <a href="forgot"> Esqueci minha senha</a>
-                </form>
+                </Form>
 
                 <a href="signup">
                     <FiLogIn />
