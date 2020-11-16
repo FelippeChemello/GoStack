427c8560ddc18da7daabd1978812e6777e43e6b2
Felippe Chemello <felippechemello@gmail.com>
2020-11-15 21:49:32 -0300

    Aula 23 - Frontend GoBarber - Criaçao de componente de rota personalizado para garantir acesso as rotas autenticadas apenas por usuarios autenticados

---
+++ GoBarber/web/src/routes/Route.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+    Redirect,
+    Route as ReactRouterDomRouteComponent,
+    RouteProps as ReactRouterDomRouteProps,
+} from 'react-router-dom';
+
+// import { Container } from './styles';
+
+import { useAuth } from '../context/AuthContext';
+
+interface RouteProps extends ReactRouterDomRouteProps {
+    isPrivate?: boolean;
+    component: React.ComponentType;
+}
+
+const Routes: React.FC<RouteProps> = ({
+    isPrivate = false,
+    component: Component,
+    ...rest
+}) => {
+    const { user } = useAuth();
+
+    return (
+        <ReactRouterDomRouteComponent
+            {...rest}
+            render={({ location }) => {
+                const isSigned = Boolean(user);
+
+                console.log(isPrivate, isSigned);
+
+                return isPrivate === isSigned ? (
+                    <Component />
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: isPrivate ? '/' : '/dashboard',
+                            state: { from: location },
+                        }}
+                    />
+                );
+            }}
+        />
+    );
+};
+
+// Lógica do return presente dentro do render
+// Se rota é privada e está logado -> renderiza a rota
+// Se a rota não é privada e não está logado -> renderiza a rota
+// Se a rota é privada e não está logado -> renderiza o SignIn
+// Se a rota não é privada e está logado -> renderiza o dashboard (Pois já fez o login antes, não precisa fazer dnv)
+
+export default Routes;
