2bd3898c8e768db4eb261aa59dc78a89d9d84992
Felippe Chemello <felippechemello@gmail.com>
2020-11-18 22:00:16 -0300

    Aula 03 - GoBarber Mobile - Rotas da aplicação com React-Navigation

--- GoBarber/mobile/src/App.tsx
+++ GoBarber/mobile/src/App.tsx
@@ -1,14 +1,19 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-// import { Container } from './styles';
+import Routes from './routes';
 
 const App: React.FC = () => {
     return (
-        <>
+        <NavigationContainer>
             <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-            <View style={{ flex: 1, backgroundColor: '#312e38' }} />
-        </>
+            <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+                <Routes />
+            </View>
+        </NavigationContainer>
     );
 };
 
