ab914d62575096e676777870f39ea00a24c06d03
Felippe Chemello <felippechemello@gmail.com>
2020-11-19 21:55:10 -0300

    Aula 07 - GoBarber Mobile - Estilização da Tela de SignIn

--- GoBarber/mobile/src/pages/SignIn/index.tsx
+++ GoBarber/mobile/src/pages/SignIn/index.tsx
@@ -1,25 +1,70 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import {
+    Container,
+    Title,
+    ForgotPassword,
+    ForgotPasswordText,
+    CreateAccountButton,
+    CreateAccountButtonText,
+} from './styles';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const SignIn: React.FC = () => {
     return (
-        <Container>
-            <Image source={logoImg} />
+        <>
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                enabled
+            >
+                <ScrollView
+                    contentContainerStyle={{ flex: 1 }}
+                    keyboardShouldPersistTaps="handled"
+                >
+                    <Container>
+                        <Image source={logoImg} />
 
-            <Title>Faça seu login</Title>
+                        {/* Os Textos não são animados no RN, como utilizamos o KeyboardAvoidingView que causa uma animação para mostrar o teclado, devemos utilizar uma View ao redor de textos para conseguir animar  */}
+                        <View>
+                            <Title>Faça seu login</Title>
+                        </View>
 
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
+                        <Input name="email" icon="mail" placeholder="E-mail" />
+                        <Input
+                            name="password"
+                            icon="lock"
+                            placeholder="Senha"
+                        />
 
-            <Button onPress={() => console.log('Entrar')}> Entrar </Button>
-        </Container>
+                        <Button onPress={() => console.log('Entrar')}>
+                            {' '}
+                            Entrar{' '}
+                        </Button>
+
+                        <ForgotPassword
+                            onPress={() => console.log('Esqueci minha senha')}
+                        >
+                            <ForgotPasswordText>
+                                Esqueci minha senha
+                            </ForgotPasswordText>
+                        </ForgotPassword>
+                    </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
+            <CreateAccountButton onPress={() => console.log('Criar conta')}>
+                <Icon name="log-in" size={20} color="#ff9000" />
+
+                <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+            </CreateAccountButton>
+        </>
     );
 };
 
