d244bf33bf3ab87b79ad0bc8ce9f91e87fa5dea1
Felippe Chemello <felippechemello@gmail.com>
2020-11-30 20:43:44 -0300

    Aula 11 - GoBarber Mobile - Adicionando cor à borda e ao icone do input quando estiver focado e/ou preenchido

--- GoBarber/mobile/src/components/Input/index.tsx
+++ GoBarber/mobile/src/components/Input/index.tsx
@@ -1,8 +1,10 @@
 import React, {
     forwardRef,
+    useCallback,
     useEffect,
     useImperativeHandle,
     useRef,
+    useState,
 } from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
@@ -33,6 +35,20 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
     );
     const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
     const inputElementRef = useRef<any>(null);
+    const [isFocused, setIsFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
+
+    const handleInputFocus = useCallback(() => setIsFocused(true), []);
+
+    const handleInputBlur = useCallback(() => {
+        setIsFocused(false);
+
+        if (inputValueRef.current.value) {
+            setIsFilled(true);
+        } else {
+            setIsFilled(false);
+        }
+    }, []);
 
     useEffect(() => {
         registerField({
@@ -59,8 +75,12 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
     });
 
     return (
-        <Container>
-            <Icon name={icon} size={20} color="#666360" />
+        <Container isFocused={isFocused}>
+            <Icon
+                name={icon}
+                size={20}
+                color={isFocused || isFilled ? '#ff9000' : '#666360'}
+            />
 
             <TextInput
                 {...rest}
@@ -71,6 +91,8 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
                 onChangeText={value => {
                     inputValueRef.current.value = value;
                 }}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
             />
         </Container>
     );
