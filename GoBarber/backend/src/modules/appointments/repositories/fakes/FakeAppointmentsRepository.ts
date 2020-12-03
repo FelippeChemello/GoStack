081f7c4d968117b12d33e03e42b73124657a4b24
Felippe Chemello <felippechemello@gmail.com>
2020-12-02 21:58:24 -0300

    Aula 14 e 15 - GoBarber Arquitetura e Testes Backend - Mockando (Criando Fake) o repositorio de appointments para nao conectar no banco de dados e criando primeiro teste unitario

---
+++ GoBarber/backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,33 @@
+import { uuid } from 'uuidv4';
+
+import InterfaceAppointmentsRepository from '@modules/appointments/repositories/InterfaceAppointmentsRepository';
+import InterfaceCreateAppointmentDTO from '@modules/appointments/dtos/InterfaceCreateAppointmentDTO';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+class AppointmentsRepository implements InterfaceAppointmentsRepository {
+    private appointments: Appointment[] = [];
+
+    public async findByDate(date: Date): Promise<Appointment | undefined> {
+        const findAppointment = this.appointments.find(
+            appointment => appointment.date === date,
+        );
+
+        return findAppointment;
+    }
+
+    public async createAndSave({
+        provider,
+        date,
+    }: InterfaceCreateAppointmentDTO): Promise<Appointment> {
+        const appointment = new Appointment();
+
+        Object.assign(appointment, { id: uuid(), date, providerId: provider });
+
+        this.appointments.push(appointment);
+
+        return appointment;
+    }
+}
+
+export default AppointmentsRepository;
--- GoBarber/backend/src/modules/appointments/services/CreateAppointmentService.test.ts
+++ GoBarber/backend/src/modules/appointments/services/CreateAppointmentService.test.ts
@@ -1,3 +1,23 @@
-test('Sum two number', () => {
-    expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+    it('Should be able to create a new appointment', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointment = await createAppointment.execute({
+            date: new Date(),
+            provider: '1234567',
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.providerId).toBe('1234567');
+    });
+
+    it('Should not be able to create two appointments on same date and time', () => {});
 });
