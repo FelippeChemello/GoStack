f8ebe7d91376d4a034a5928a2647e9f5a2a628a1
Felippe Chemello <felippechemello@gmail.com>
2020-12-02 22:52:36 -0300

    Aula 20 - GoBarber Arquitetura e Testes Backend - Criando teste de falha ao autenticar com e-mail inexistente e com senha incorreta

--- GoBarber/backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ GoBarber/backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -31,4 +31,43 @@ describe('AuthenticateUser', () => {
         expect(response).toHaveProperty('token');
         expect(response.user).toBe(user);
     });
+
+    it('Should not be able to authenticate due user does not exist', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        expect(
+            new AuthenticateUserService(
+                fakeUsersRepository,
+                fakeHashProvider,
+            ).execute({
+                email: 'johndoe@example.com',
+                password: '123456',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('Should not be able to authenticate due password is incorrect', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        await new CreateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        ).execute({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        expect(
+            new AuthenticateUserService(
+                fakeUsersRepository,
+                fakeHashProvider,
+            ).execute({
+                email: 'johndoe@example.com',
+                password: '654321',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
 });
