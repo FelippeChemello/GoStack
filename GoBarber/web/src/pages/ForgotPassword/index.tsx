9b04dad11a243123a0c68bfa7e55628e4cab5cb5
Felippe Chemello <felippechemello@gmail.com>
2020-12-14 10:42:31 -0300

    Aula 08 - GoBarber Finalizando Frontend - Criando layout da pagina de recuperaçao de senha

--- GoBarber/web/src/pages/ForgotPassword/index.tsx
+++ GoBarber/web/src/pages/ForgotPassword/index.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useContext, useRef } from 'react';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import { FiLock, FiLogIn, FiMail } from 'react-icons/fi';
+import { FiLogIn, FiMail } from 'react-icons/fi';
 import { Link, useHistory } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
@@ -12,22 +12,19 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import getValidationErrors from '../../utils/getValidationErrors';
-import { useAuth } from '../../context/AuthContext';
 import { useToast } from '../../context/ToastContext';
 
-interface SignInFormData {
+interface ForgotPasswordFormData {
     email: string;
-    password: string;
 }
 
-const SignIn: React.FC = () => {
+const ForgotPassword: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
-    const { signIn, user } = useAuth();
     const { addToast } = useToast();
     const history = useHistory();
 
     const handleSubmit = useCallback(
-        async (data: SignInFormData) => {
+        async (data: ForgotPasswordFormData) => {
             formRef.current?.setErrors({});
 
             try {
@@ -35,16 +32,10 @@ const SignIn: React.FC = () => {
                     email: Yup.string()
                         .required('E-mail obrigatório')
                         .email('Digite um e-mail válido'),
-                    password: Yup.string().required('Senha obrigatória'),
                 });
 
                 await schema.validate(data, {
                     abortEarly: false,
-                });
-
-                await signIn({
-                    email: data.email,
-                    password: data.password,
                 });
 
                 history.push('/dashboard');
@@ -58,13 +49,13 @@ const SignIn: React.FC = () => {
 
                 addToast({
                     type: 'error',
-                    title: 'Erro ao autenticar',
+                    title: 'Erro ao recuperar senha',
                     description:
-                        'Ocorreu um erro ao realizar o login, verifique as credenciais.',
+                        'Ocorreu um erro ao tentar recuperar a senha, tente novamente.',
                 });
             }
         },
-        [signIn, addToast, history],
+        [addToast, history],
     );
 
     return (
@@ -74,7 +65,7 @@ const SignIn: React.FC = () => {
                     <img src={logoImg} alt="GoBarber" />
 
                     <Form ref={formRef} onSubmit={handleSubmit}>
-                        <h1>Faça seu login</h1>
+                        <h1>Recuperar senha</h1>
 
                         <Input
                             placeholder="E-mail"
@@ -82,21 +73,12 @@ const SignIn: React.FC = () => {
                             icon={FiMail}
                         />
 
-                        <Input
-                            type="password"
-                            placeholder="Senha"
-                            name="password"
-                            icon={FiLock}
-                        />
-
-                        <Button type="submit"> Entrar </Button>
-
-                        <Link to="/forgot-password"> Esqueci minha senha</Link>
+                        <Button type="submit"> Recuperar </Button>
                     </Form>
 
-                    <Link to="/signup">
+                    <Link to="/">
                         <FiLogIn />
-                        Criar conta
+                        Voltar ao login
                     </Link>
                 </AnimationContainer>
             </Content>
@@ -105,4 +87,4 @@ const SignIn: React.FC = () => {
     );
 };
 
-export default SignIn;
+export default ForgotPassword;
