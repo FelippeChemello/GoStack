f99d47bf082e0a451f2da68e984b506dff2d08f4
Felippe Chemello <felippechemello@gmail.com>
2020-12-14 12:56:37 -0300

    Aula 13 - GoBarber Finalizando Frontend - Criando layout de atendimento a seguir

--- GoBarber/web/src/pages/Dashboard/index.tsx
+++ GoBarber/web/src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+    Container,
+    Header,
+    HeaderContent,
+    Profile,
+    Content,
+    Schedule,
+    NextAppointment,
+    Calendar,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
 
 import { useAuth } from '../../context/AuthContext';
@@ -29,6 +38,33 @@ const Dashboard: React.FC = () => {
                     </button>
                 </HeaderContent>
             </Header>
+
+            <Content>
+                <Schedule>
+                    <h1>Horários agendados</h1>
+                    <p>
+                        <span>Hoje</span>
+                        <span>Dia 06</span>
+                        <span>Segunda-feira</span>
+                    </p>
+
+                    <NextAppointment>
+                        <strong>Atendimento a seguir</strong>
+
+                        <div>
+                            <img src={user.avatarUrl} alt={user.name} />
+
+                            <strong>{user.name}</strong>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                        </div>
+                    </NextAppointment>
+                </Schedule>
+
+                <Calendar />
+            </Content>
         </Container>
     );
 };
