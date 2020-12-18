ddabc7afc0a9e052e59b0930de06f9ebe89e5852
Felippe Chemello <felippechemello@gmail.com>
2020-12-17 22:06:43 -0300

    Aula 16 - GoBarber Finalizando Mobile - Criando tela de finalizaçao de agendamento & Resetando Stack de navegaçao para nao conseguir voltar para tela anterior

--- GoBarber/mobile/src/pages/AppointmentCreated/index.tsx
+++ GoBarber/mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+    Container,
+    Title,
+    Description,
+    OkButton,
+    OkButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-    return <View></View>;
+    const { reset } = useNavigation();
+
+    const handleOkPressed = useCallback(() => {
+        reset({
+            routes: [
+                {
+                    name: 'Dashboard',
+                },
+            ],
+            index: 0,
+        });
+    }, [reset]);
+
+    return (
+        <Container>
+            <Icon name="check" size={80} color="#04d361" />
+
+            <Title>Agendamento concluído</Title>
+
+            <Description>blah</Description>
+
+            <OkButton onPress={handleOkPressed}>
+                <OkButtonText>Ok</OkButtonText>
+            </OkButton>
+        </Container>
+    );
 };
 
 export default AppointmentCreated;
