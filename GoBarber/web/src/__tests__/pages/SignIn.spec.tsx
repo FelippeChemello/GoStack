01c8ac32da49f093e1c2e4f57b128abe96cedd97
Felippe Chemello <felippechemello@gmail.com>
2020-12-21 20:16:13 -0300

    Aula 04 - GoBarber Testes E2E - Iniciando teste da tela de Sign In

--- GoBarber/web/src/__tests__/pages/SignIn.spec.tsx
+++ GoBarber/web/src/__tests__/pages/SignIn.spec.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import SignIn from '../../pages/SignIn';
 
-// Quando este modulo for importado retornará o que estiver dentro do return
-// Neste caso criamos o useHistory e dissemos que ele é uma função vazia através de 'jest.fn'
+const mockedHistoryPush = jest.fn();
+
 jest.mock('react-router-dom', () => {
     return {
-        useHistory: jest.fn(),
+        useHistory: () => ({
+            push: mockedHistoryPush,
+        }),
         Link: ({ children }: { children: React.ReactNode }) => children,
     };
 });
 
 describe('SignIn Page', () => {
     it('should be able to sign in', () => {
-        const { debug } = render(<SignIn />);
+        const { getByPlaceholderText, getByText } = render(<SignIn />);
 
-        debug();
+        const emailField = getByPlaceholderText('E-mail');
+        const passwordField = getByPlaceholderText('Senha');
+        const buttonElement = getByText('Entrar');
+
+        fireEvent.change(emailField, {
+            target: { value: 'johndoe@example.com' },
+        });
+
+        fireEvent.change(passwordField, {
+            target: { value: '123456' },
+        });
+
+        fireEvent.click(buttonElement);
+
+        expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
     });
 });
