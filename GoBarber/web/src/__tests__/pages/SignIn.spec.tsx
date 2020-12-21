d97fd07f03e4bad8b38a8b149430ef37a8c95c96
Felippe Chemello <felippechemello@gmail.com>
2020-12-21 20:42:24 -0300

    Aula 06 - GoBarber Testes E2E - Finalizando testes da pagina SignIn

--- GoBarber/web/src/__tests__/pages/SignIn.spec.tsx
+++ GoBarber/web/src/__tests__/pages/SignIn.spec.tsx
@@ -4,6 +4,8 @@ import { render, fireEvent, waitFor } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
 
 const mockedHistoryPush = jest.fn();
+const mockedSignIn = jest.fn();
+const mockedAddToast = jest.fn();
 
 jest.mock('react-router-dom', () => {
     return {
@@ -18,13 +20,27 @@ jest.mock('../../hooks/AuthContext', () => {
     return {
         useAuth: () => {
             return {
-                signIn: jest.fn(),
+                signIn: mockedSignIn,
+            };
+        },
+    };
+});
+
+jest.mock('../../hooks/ToastContext', () => {
+    return {
+        useToast: () => {
+            return {
+                addToast: mockedAddToast,
             };
         },
     };
 });
 
 describe('SignIn Page', () => {
+    beforeEach(() => {
+        mockedHistoryPush.mockClear();
+    });
+
     it('should be able to sign in', async () => {
         const { getByPlaceholderText, getByText } = render(<SignIn />);
 
@@ -44,6 +60,58 @@ describe('SignIn Page', () => {
 
         await waitFor(() => {
             expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+        });
+    });
+
+    it('should not be able to login with invalid credentials', async () => {
+        const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+        const emailField = getByPlaceholderText('E-mail');
+        const passwordField = getByPlaceholderText('Senha');
+        const buttonElement = getByText('Entrar');
+
+        fireEvent.change(emailField, {
+            target: { value: 'invalid-email' },
+        });
+
+        fireEvent.change(passwordField, {
+            target: { value: '123456' },
+        });
+
+        fireEvent.click(buttonElement);
+
+        await waitFor(() => {
+            expect(mockedHistoryPush).not.toHaveBeenCalled();
+        });
+    });
+
+    it('should not be able to login with unknown error, likely api error, showing toast error', async () => {
+        mockedSignIn.mockImplementation(() => {
+            throw new Error();
+        });
+
+        const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+        const emailField = getByPlaceholderText('E-mail');
+        const passwordField = getByPlaceholderText('Senha');
+        const buttonElement = getByText('Entrar');
+
+        fireEvent.change(emailField, {
+            target: { value: 'johndoe@example.com' },
+        });
+
+        fireEvent.change(passwordField, {
+            target: { value: '123456' },
+        });
+
+        fireEvent.click(buttonElement);
+
+        await waitFor(() => {
+            expect(mockedAddToast).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    type: 'error',
+                }),
+            );
         });
     });
 });
