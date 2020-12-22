7ed6eef03be224e8c34d6d095df46e9017954a65
Felippe Chemello <felippechemello@gmail.com>
2020-12-21 21:25:06 -0300

    Aula 09 - GoBarber Testes E2E - Criando Mock da API & Criando mock para localstorage & finalizando primeiro teste do hook de autenticaçao (signIn)

--- GoBarber/web/src/__tests__/hooks/AuthContext.spec.tsx
+++ GoBarber/web/src/__tests__/hooks/AuthContext.spec.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
+import MockAdapter from 'axios-mock-adapter';
 
 import { AuthProvider, useAuth } from '../../hooks/AuthContext';
 
+import api from '../../services/api';
+
+const apiMock = new MockAdapter(api);
+
 describe('Auth Hook', () => {
-    it('should be able to SignIn', () => {
-        const { result } = renderHook(() => useAuth(), {
+    it('should be able to SignIn', async () => {
+        const apiResponse = {
+            user: {
+                id: 'userId',
+                name: 'John Doe',
+                email: 'johndoe@example.com.br',
+            },
+            token: 'token-123',
+        };
+
+        apiMock.onPost('sessions').reply(200, apiResponse);
+
+        const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+
+        const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
             wrapper: AuthProvider,
         });
 
@@ -14,6 +32,16 @@ describe('Auth Hook', () => {
             password: '123456',
         });
 
+        await waitForNextUpdate();
+
+        expect(setItemSpy).toHaveBeenCalledWith(
+            '@GoBarber:token',
+            apiResponse.token,
+        );
+        expect(setItemSpy).toHaveBeenCalledWith(
+            '@GoBarber:user',
+            JSON.stringify(apiResponse.user),
+        );
         expect(result.current.user.email).toEqual('johndoe@example.com.br');
     });
 });
