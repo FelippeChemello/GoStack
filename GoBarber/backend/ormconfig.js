30968e852b9e41e3fa6061733aba1c06e7fa87bc
Felippe Chemello <felippechemello@gmail.com>
2021-01-05 22:03:16 -0300

    Aula 07 - Deploy Node - Adicionando Username e Password para conexao com mongo

--- GoBarber/backend/ormconfig.js
+++ GoBarber/backend/ormconfig.js
@@ -34,6 +34,8 @@ module.exports = [
         host: process.env.MONGO_HOST,
         port: process.env.MONGO_PORT,
         database: process.env.MONGO_DATABASE,
+        username: process.env.MONGO_USERNAME,
+        password: process.env.MONGO_PASSWORD,
         useUnifiedTopology: true,
         entities: databasePaths[process.env.APP_ENV].schemas,
     },
