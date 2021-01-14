c86d3bfa5c7206f47983888cf8ffe6af344c3bb2
Felippe Chemello <felippechemello@gmail.com>
2021-01-13 22:41:24 -0300

    Aula 04 - Deploy React Native - Controlando a splashscreen na aplicaçao

--- GoBarber/mobile/src/App.tsx
+++ GoBarber/mobile/src/App.tsx
@@ -1,14 +1,19 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 
 import AppProvider from './hooks';
 
 import Routes from './routes';
 
 const App: React.FC = () => {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
+
     return (
         <NavigationContainer>
             <StatusBar
